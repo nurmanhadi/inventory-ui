18a1188815ed86b4d5939963f595d35e2566a793
Nurman Hadi <manzdeveloper@gmail.com>
2026-05-24 10:57:37 +0700

    delete search page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,16 +5,20 @@ import { ProductResponse } from "../apis/dtos/product-dto";
 import Pagination from "../components/Pagination";
 import Loading from "../components/Loading";
 import AddProduct from "../components/products/AddProduct";
+import { useSearchParams } from "@solidjs/router";
 
 const ProductPage: Component<{}> = (props) => {
+    const [query, setQuery] = useSearchParams()
     const [products, setProducts] = createSignal<ProductResponse[]>();
     const [page, setPage] = createSignal<number>(1);
     const [totalPages, setTotalPages] = createSignal<number>(1);
+    const search = Array.isArray(query.s) ? query.s[0] : query.s
 
-    const fetchProducts = async (page: number) => {
+
+    const fetchProducts = async (page: number, search?: string) => {
         const size: number = 20;
         try {
-            const web = await apiGetProducts(page, size);
+            const web = await apiGetProducts(page, size, search);
             setProducts(web.data.contents);
             setTotalPages(web.data.total_pages);
         } catch (error) {
@@ -22,12 +26,14 @@ const ProductPage: Component<{}> = (props) => {
         }
     }
     createEffect(() => {
-        fetchProducts(page());
+        fetchProducts(page(), search);
     })
     return (
         <Show when={products()} fallback={<Loading />}>
-            <AddProduct onSuccess={() => fetchProducts(page())} />
-            <hr class="my-6" />
+            <Show when={!search} fallback>
+                <AddProduct onSuccess={() => fetchProducts(page())} />
+                <hr class="my-6" />
+            </Show>
             <ProductList products={products()!} />
             <div class="flex justify-end mt-4">
                 <Pagination page={page()} totalPages={totalPages()} onPageChange={setPage} />
